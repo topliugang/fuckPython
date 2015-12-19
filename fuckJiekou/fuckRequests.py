385de71d3c169ee67a7d94fc22be66824b045c15
topliugang <335762225@qq.com>
2015-12-19 21:12:18 +0800

    away from vc

---
+++ fuckJiekou/fuckRequests.py
@@ -0,0 +1,15 @@
+﻿ # -*- coding:utf-8 -*- 
+import requests
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+url = 'http://www.baidu.com/'
+url2 = 'http://wanimal1983.tumblr.com/'
+
+req = requests.get(url2)
+page = req.text
+print page.decode()
+
+
+
